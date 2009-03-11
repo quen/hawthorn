757e27633fa43eaa53de015967f4acfe67789a27
sam marshall <sam@leafdigital.com>
2009-03-11 22:52:41 +0000

    Implemented popup window fully in HTML example (there's no way for it to get a new key yet, but you can't do that in HTML anyway).

--- js/hawthorn.js
+++ js/hawthorn.js
@@ -22,8 +22,8 @@ along with hawthorn.  If not, see <http://www.gnu.org/licenses/>.
 var hawthorn =
 {
 	id : 1,
-	handlers :
-	[],
+	handlers : [],
+	currentServer : null,
 
 	init : function(servers,preferred)
 	{
@@ -37,15 +37,20 @@ var hawthorn =
 
 	addTag : function(path)
 	{
+		var currentId = this.id;
+		if(!hawthorn.currentServer)
+		{
+			hawthorn.getHandler(currentId).failure('Not inited');
+			this.id++;
+		}
 		var head = document.getElementsByTagName("head")[0];
 		var newScript = document.createElement('script');
-		var currentId = this.id;
 		newScript.id = 'hawthorn_script' + this.id;
 		newScript.type = 'text/javascript';
 		newScript.src = this.currentServer + path + "&id=" + this.id;
 		newScript.onerror = function()
 		{
-			hawthorn.getHandler(currentId).failure('Load error');
+			hawthorn.getHandler(currentId).failure('Error accessing chat server');
 		};
 		head.appendChild(newScript);
 		this.id++;
@@ -71,7 +76,7 @@ var hawthorn =
 	},
 
 	getRecent : function(channel,user,displayName,keyTime,key,maxAge,maxNumber,
-			continuation,failure)
+		maxNames,continuation,failure)
 	{
 		this.handlers.push(
 		{
@@ -81,13 +86,14 @@ var hawthorn =
 		});
 		this.addTag('hawthorn/getRecent?channel=' + channel + '&user=' + user
 				+ '&displayname=' + escape(displayName) + '&keytime=' + keyTime
-				+ "&key=" + key + "&maxage=" + maxAge + "&maxnumber=" + maxNumber);
+				+ "&key=" + key + "&maxage=" + maxAge + "&maxnumber=" + maxNumber
+				+ "&maxnames=" + maxNames);
 	},
 
-	getRecentComplete : function(id,messages)
+	getRecentComplete : function(id, messages, names)
 	{
 		this.removeTag(id);
-		this.getHandler(id, 'getRecent').continuation(messages);
+		this.getHandler(id, 'getRecent').continuation(messages, names);
 	},
 
 	getRecentError : function(id,error)
@@ -175,10 +181,10 @@ var hawthorn =
 				+ "&key=" + key + "&lasttime=" + lastTime);
 	},
 
-	waitForMessageComplete : function(id,lastTime,messages)
+	waitForMessageComplete : function(id, lastTime, messages, names)
 	{
 		this.removeTag(id);
-		this.getHandler(id, 'waitForMessage').continuation(lastTime, messages);
+		this.getHandler(id, 'waitForMessage').continuation(lastTime, messages, names);
 	},
 
 	waitForMessageError : function(id,error)
@@ -210,5 +216,332 @@ var hawthorn =
 	{
 		this.removeTag(id);
 		this.getHandler(id, 'getLog').failure(error);
+	},
+
+	openPopup : function(url,channel,user,displayName,keyTime,key,title)
+	{
+		this.chatWindow = window.open(url+'?channel='+channel+'&user='+user+
+			'&displayName='+escape(displayName)+'&keyTime='+keyTime+'&key='+key+
+			'&title='+escape(title)+'&server='+escape(this.currentServer),'_'+channel,
+			'width=500,height=400,menubar=no,'+
+			'toolbar=no,location=no,directories=no,status=no,resizable=yes,'+
+			'scrollbars=no');
 	}
+}
+
+/**
+ * Standard implementation of popup window used to display chat in a channel.
+ * This constructor should be called from the page that defines the popup.
+ * It inits the hawthorn system and starts listening for events.
+ * @constructor
+ */
+function HawthornPopup()
+{
+	this.server = this.getPageParam('server');
+	hawthorn.init([this.server], 0);
+
+	this.channel = this.getPageParam('channel');
+	this.user = this.getPageParam('user');
+	this.displayName = this.getPageParam('displayName');
+	this.keyTime = this.getPageParam('keyTime');
+	this.key = this.getPageParam('key');
+	document.title = this.getPageParam('title');
+	this.maxAge = 10*60*1000; // 10 minutes old (default)
+	this.maxNumber = 10; // 10 messages (default)
+	this.lastTime = 0;
+	this.lastDisplayTime = '';
+
+	this.strJoined = ' joined the chat';
+	this.strLeft = ' left the chat';
+	this.strError = 'A system error occurred';
+
+	this.initLayout();
+	var p = this;
+	this.textBox.onkeypress=function(e)
+	{
+		var key;
+		if(window.event)
+		{
+			key = e.keyCode;
+		}
+		else
+		{
+			key = e.which;
+		}
+		if(key==13)
+		{
+		  p.say();
+		}
+	};
+	this.namesArea.present = new Object();
+	this.closeButton.onclick=function()
+	{
+		p.left=true;
+		hawthorn.leave(p.channel, p.user, p.displayName, p.keyTime,
+			p.key, function() { window.close(); }, function(error) { window.close(); });
+	};
+	this.startWait();
+}
+
+HawthornPopup.prototype.getPageParam = function(name)
+{
+	var re = new RegExp("[?&]"+name+"=([^&#]*)");
+	var matches = re.exec(window.location.href);
+	if(matches)
+	{
+		return unescape(matches[1]);
+	}
+	else
+	{
+		throw "Window requires parameter '"+name+"'";
+	}
+}
+
+HawthornPopup.prototype.startWait = function()
+{
+	if(this.left)
+	{
+		return;
+	}
+	var p = this;
+	var ok = function(lastTime,messages,names)
+	{
+		p.lastTime = lastTime;
+		for(var i = 0; i < messages.length; i++)
+		{
+			var message = messages[i];
+			if(message.type == 'JOIN')
+			{
+				p.addJoin(message.time, message.user, message.displayName,
+					message.user == p.user);
+			}
+			else if(message.type == 'LEAVE')
+			{
+				p.addLeave(message.time, message.user, message.displayName,
+					message.user == p.user);
+			}
+			else if(message.type == 'SAY')
+			{
+				p.addMessage(message.time, message.user, message.displayName,
+					message.text, message.user == p.user)
+			}
+		}
+		for(var i = 0; i < names.length; i++)
+		{
+			var name=names[i];
+			p.addName(name.user, name.displayName);
+		}
+		p.startWait();
+	};
+	var fail = function(error)
+	{
+		p.addError(error);
+	};
+
+	if(this.lastTime == 0)
+	{
+		hawthorn.waitForMessageFirst(this.channel, this.user, this.displayName,
+			this.keyTime, this.key, this.maxAge, this.maxNumber, ok, fail);
+	}
+	else
+	{
+		hawthorn.waitForMessage(this.channel, this.user, this.displayName,
+				this.keyTime, this.key, this.lastTime, ok, fail);
+	}
+}
+
+/**
+ * Sets data variables that hold the three main components (used later by
+ * default implementations of addMessage etc).
+ */
+HawthornPopup.prototype.initLayout = function()
+{
+	this.mainArea = document.getElementById("main");
+	this.namesArea = document.getElementById("names");
+	this.textBox = document.getElementById("textbox");
+	this.closeButton = document.getElementById('closebutton');
+}
+
+/**
+ * Adds the given user to the name list (alphabetically sorted).
+ * @param user User ID
+ * @param displayName Display name for user
+ */
+HawthornPopup.prototype.addName = function(user,displayName)
+{
+	var el = this.namesArea.present[user];
+	if (el)
+	{
+		return;
+	}
+
+	var newEl = document.createElement('div');
+	newEl.className = 'name';
+	newEl.appendChild(document.createTextNode(displayName));
+	this.namesArea.present[user] = newEl;
+
+	for (var current = this.namesArea.firstChild; current != null; 
+		current = current.nextSibling)
+	{
+		if(current.className != 'name')
+		{
+			continue;
+		}
+		var currentName = current.firstChild.nodeValue;
+		if(currentName > displayName)
+		{
+			this.namesArea.insertBefore(newEl, current);
+			return;
+		}
+	}
+
+	this.namesArea.appendChild(newEl);
+}
+
+/**
+ * Removes the given user from the name list.
+ * @param user User ID
+ * @param displayName Display name for user
+ */
+HawthornPopup.prototype.removeName = function(user,displayName)
+{
+	var el = this.namesArea.present[user];
+	if(!el)
+	{
+		return;
+	}
+	this.namesArea.removeChild(el);
+	this.namesArea.present[user] = undefined;
+}
+
+/**
+ * Adds an entry to the message area.
+ * @param el Element
+ */
+HawthornPopup.prototype.addEntry = function(time,el)
+{
+	var date = new Date(time);
+	var hours = date.getHours(), mins = date.getMinutes();
+	if (hours<10)
+	{
+		hours = '0' + hours;
+	}
+	if (mins<10)
+	{
+		mins = '0' + mins;
+	}
+	var displayTime = hours+":"+mins;
+	if(displayTime != this.lastDisplayTime)
+	{
+		this.lastDisplayTime = displayTime;
+		var timeEl = document.createElement('div');
+		timeEl.className = 'timestamp';
+		var inner = document.createElement('span');
+		timeEl.appendChild(inner);
+		inner.appendChild(document.createTextNode(displayTime));
+		this.mainArea.appendChild(timeEl);
+	}
+
+	this.mainArea.appendChild(el);
+	el.scrollIntoView();
+}
+
+/**
+ * Adds a newly-received message to the message area.
+ * @param time Message time (ms since 1970)
+ * @param user User ID
+ * @param displayName Display name for user
+ * @param message Message from user
+ * @param self True if this is a message from the current user
+ */
+HawthornPopup.prototype.addMessage = function(time,user,displayName,message,self)
+{
+	var entry = document.createElement('div');
+	entry.className='entry say' + (self ? ' self' : '');
+	var inner = document.createElement('div');
+	inner.className = 'message';
+	inner.appendChild(document.createTextNode('<'));
+	var name = document.createElement('strong');
+	name.className = 'name';
+	name.appendChild(document.createTextNode(displayName));
+	inner.appendChild(name);
+	inner.appendChild(document.createTextNode('> ' + message));
+	entry.appendChild(inner);
+	this.addEntry(time, entry);
+}
+
+/**
+ * Adds a newly-received join message to the message area.
+ * @param time Message time (ms since 1970)
+ * @param user User ID
+ * @param displayName Display name for user
+ * @param self True if this is a message from the current user
+ */
+HawthornPopup.prototype.addJoin = function(time,user,displayName,self)
+{
+	var entry = document.createElement('div');
+	entry.className='entry join' + (self ? ' self' : '');
+	entry.appendChild(document.createTextNode('\u2022 '));
+	var name = document.createElement('strong');
+	name.className='name';
+	name.appendChild(document.createTextNode(displayName));
+	entry.appendChild(name);
+	entry.appendChild(document.createTextNode(this.strJoined));
+	this.addEntry(time, entry);
+	this.addName(user, displayName);
+}
+
+/**
+ * Adds a newly-received leave message to the message area.
+ * @param time Message time (ms since 1970)
+ * @param user User ID
+ * @param displayName Display name for user
+ * @param self True if this is a message from the current user
+ */
+HawthornPopup.prototype.addLeave = function(time,user,displayName,self)
+{
+	this.removeName(user, displayName);
+	var entry = document.createElement('div');
+	entry.className = 'entry leave' + (self ? ' self' : '');
+	entry.appendChild(document.createTextNode('\u2022 '));
+	var name = document.createElement('strong');
+	name.className = 'name';
+	name.appendChild(document.createTextNode(displayName));
+	entry.appendChild(name);
+	entry.appendChild(document.createTextNode(this.strLeft));
+	this.addEntry(time, entry);
+}
+
+/**
+ * Adds an error message to the message area.
+ * @param message Error message
+ */
+HawthornPopup.prototype.addError = function(message)
+{
+	var entry = document.createElement('div');
+	entry.className = 'entry error';
+	var h3 = document.createElement('h3');
+	h3.appendChild(document.createTextNode(this.strError));
+	entry.appendChild(h3);
+	var div = document.createElement('div');
+	div.appendChild(document.createTextNode(message));
+	entry.appendChild(div);
+	this.addEntry((new Date()).getTime(), entry);
+	this.mainArea.appendChild(entry);
+}
+
+/**
+ * Called when user types Return. Obtains text from the textbox and
+ * sends a message to the channel with that text.
+ */
+HawthornPopup.prototype.say = function()
+{
+	var text = this.textBox.value;
+	if(text == '')
+	{
+		return;
+	}
+	this.textBox.value = '';
+	hawthorn.say(this.channel, this.user, this.displayName, this.keyTime,
+		this.key, text, function() {}, this.addError);
 }
