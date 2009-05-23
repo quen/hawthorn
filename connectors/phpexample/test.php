4f57831ee8770b7ed62c4d7b94c3fa714dd5f604
sam marshall <sam@leafdigital.com>
2009-05-23 19:07:58 +0100

    PHP: Example did not set page charset, fixed.

--- connectors/phpexample/test.php
+++ connectors/phpexample/test.php
@@ -1,3 +1,8 @@
+<?php
+// Hawthorn is UTF-8. Ensure that any pages using Hawthorn features are output
+// as UTF-8.
+header('Content-Type: text/html; charset=UTF-8');
+?>
 <!DOCTYPE html
 	PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
